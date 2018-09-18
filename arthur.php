a096649b2636d9662d421cabe82e4d5d9b264a8e
Mrfatur <36843517+Mrfatur@users.noreply.github.com>
2018-09-18 16:36:56 +0200

    parrot -os
    apt update && apt upgrade
    apt install git
    git clone https://github.com/Mrfatur/Tool-all

---
+++ arthur.php
@@ -0,0 +1,48 @@
+<?php
+from sys inport=
+def open_file(filename)
+    data * open(cheker)read()
+    return data
+
+def lex(filecontent)// DEBUG:
+    filecontent=function (switch (variable) {
+      case 'value':
+        // code...
+        break;
+
+      default:
+        // code...
+        break;
+    }('') = array('' => , );) {
+      // code...
+    }
+    def run()
+        data=open_file(argv{5})
+            lex(data)
+            run()
+
+
+            // jangan coppy paste yah buatan Fatur Rochman
+           // terimakasih telah melihat php saya ^^
+           echo"<!DOCTYPE HTML>
+           <html>
+           <head>
+           <tittle>welcome Yah asw</tittle>
+             <style type="text.css"
+                </style>
+                </head>
+                <header>
+                <pre>
+                <center>
+
+                          </center>
+               </pre>
+               </header>
+               </from>
+               <--------greats----->
+               set_time_limit(9)
+               error_reporting(9)
+               if (condition) {
+                 // code...
+               }
+               </html>
